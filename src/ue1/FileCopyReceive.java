e63efb04259ae321de45f5ec980f22f6f166e534
Simon <s0544318@htw-berlin.de>
2021-04-25 16:31:45 +0200

    File copy over TCP

---
+++ src/ue1/FileCopyReceive.java
@@ -0,0 +1,53 @@
+package ue1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileCopyReceive {
+
+	static public void main (final String[] args) throws IOException {
+
+		final Path sinkPath = Paths.get(args[0]);
+		if (sinkPath.getParent() != null && !Files.isDirectory(sinkPath.getParent())) throw new IllegalArgumentException(sinkPath.toString());
+
+		final String ipAndPortAsString = args[1];
+		
+		// if(ipAndPortAsString.contains(":")) throw new IllegalArgumentException(ipAndPortAsString);		
+		
+		String ip;
+		int port;
+		
+		String[] splitResult = ipAndPortAsString.split(":");
+		
+		if(splitResult.length != 2) throw new IllegalArgumentException(ipAndPortAsString);
+		
+		ip = splitResult[0];
+		try {			
+			port = Integer.parseInt(splitResult[1]);
+		} catch(NumberFormatException nfe) {
+			throw new IllegalArgumentException(ipAndPortAsString);
+		}
+
+		Socket socket = new Socket(ip, port);
+		
+		try (OutputStream fos = Files.newOutputStream(sinkPath)) {
+			try(InputStream in = socket.getInputStream()) {
+				final byte[] buffer = new byte[0x10000];
+				for (int bytesRead = in.read(buffer); bytesRead != -1; bytesRead = in.read(buffer)) {					
+					fos.write(buffer, 0, bytesRead);
+				}
+			}
+		} catch(IOException ioe) {		
+			System.out.println(ioe);
+		}
+		
+		socket.close();
+		
+		System.out.println("Receiving done");
+	}
+}
---
+++ src/ue1/FileCopySend.java
@@ -0,0 +1,48 @@
+package ue1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileCopySend {
+
+	static public void main (final String[] args) throws IOException {
+
+		final Path sourcePath = Paths.get(args[0]);
+		if (!Files.isReadable(sourcePath)) throw new IllegalArgumentException(sourcePath.toString());
+
+		final String portAsString = args[1];
+		int port;
+		try {			
+			port = Integer.parseInt(portAsString);
+		} catch(NumberFormatException nfe) {
+			throw new IllegalArgumentException(portAsString);
+		}
+		
+		ServerSocket service = new ServerSocket(port);
+		
+		System.out.println("Waiting for connections...");
+        Socket connection = service.accept();
+		System.out.println("Connection established");
+
+		try (InputStream fis = Files.newInputStream(sourcePath)) {			
+			final byte[] buffer = new byte[0x10000];
+			OutputStream outputStream = connection.getOutputStream();
+			for (int bytesRead = fis.read(buffer); bytesRead != -1; bytesRead = fis.read(buffer)) {
+				outputStream.write(buffer, 0, bytesRead);
+			}			
+			outputStream.close();
+		} catch(IOException ioe) {
+			System.out.println(ioe);
+		}
+
+		service.close();
+		
+		System.out.println("Sending done");
+	}
+}
